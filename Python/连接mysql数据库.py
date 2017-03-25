f143a62813ceb18b5d2bb73e2ceb79298c998b91
Taoey <741494582@qq.com>
2017-03-25 13:57:00 +0800

    MyCode

---
+++ Python/连接mysql数据库.py
@@ -0,0 +1,13 @@
+﻿#coding=utf-8
+'''
+blog : http://www.cnblogs.com/lhuan/p/6160023.html
+'''
+
+import pymysql
+
+# main
+db=pymysql.connect('localhost','root','password','YourdbName')
+cursor=db.cursor()
+cursor.execute('SELECT VERSION()')
+data = cursor.fetchone()
+print ("Database version : %s " % data)
