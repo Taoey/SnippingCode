0f896ff2e96e5bdc95e4afa8e8b4b2d5ba8223c2
Taoey <741494582@qq.com>
2017-03-30 21:34:01 +0800

    upload

---
+++ C++/动态二维数组.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+
+
+int main(){
+	int row,column;  
+	cin>>row>>column;  
+
+	//方法一  
+	//申请空间  
+	int ** a = new int *[row];  
+	for(int i = 0;i < row;i++)  
+		a[i] = new int[column];  
+
+	//使用空间  
+	for(int j = 0;j < row;j++)  
+		for(int k = 0;k< column;k++)  
+			a[j][k] = rand()%100;  
+
+	for(int j = 0;j < row;j++)  
+	{  
+		cout<<endl;  
+		for(int k = 0;k< column;k++)  
+		{  
+			a[j][k] = rand()%100;  
+			cout<<a[j][k]<<"     ";  
+		}  
+	}  
+
+	//释放空间  
+	for(int i = 0;i < row;i++)  
+	{  
+		delete a[i];  
+		a[i] = NULL;  
+	}  
+	delete [row]a;  
+	a = NULL;     
+
+
+	return 0;
+}
